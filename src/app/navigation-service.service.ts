e9838ef7ef7423b7689114afabcebd3b2316a568
Claude Glauser <public@claude-glauser.ch>
2017-07-16 18:12:02 +0200

    selectedThema

--- src/app/navigation-service.service.ts
+++ src/app/navigation-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject }    from 'rxjs/Subject';
 import { Kategorie } from './model/Kategorie';
+import {Themenfeld} from './model/Themenfeld'
 
 @Injectable()
 export class NavigationServiceService {
@@ -11,8 +12,17 @@ export class NavigationServiceService {
 
   public selectedKategoryObs = this.selectedKategorieSub.asObservable();
 
+  private selectThemenfeldSub = new Subject<Themenfeld>();
+
+  public selectedThemenfeldBs = this.selectThemenfeldSub.asObservable();
+
   public selectCategory(kategorie: Kategorie) {
     this.selectedKategorieSub.next(kategorie);
+  }
+
+  public selectThemenfeld(themenfeld: Themenfeld)
+  {
+    this.selectThemenfeldSub.next(themenfeld);
   }
   
 }
