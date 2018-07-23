66176023389cb6c9c0cac81f31613acee7f7e578
Claude Glauser <public@claude-glauser.ch>
2018-07-23 22:20:19 +0200

    unit tests zwischenstand

--- src/app/container/container.component.spec.ts
+++ src/app/container/container.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {RouterModule} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ContainerComponent } from './container.component';
 
@@ -8,6 +10,7 @@ describe('ContainerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ ContainerComponent ]
     })
     .compileComponents();
--- src/app/safe-html.pipe.spec.ts
+++ src/app/safe-html.pipe.spec.ts
@@ -1,8 +1,26 @@
 import { SafeHtmlPipe } from './safe-html.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TestBed, async, inject } from '@angular/core/testing';
 
 describe('SafeHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafeHtmlPipe();
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+      
+      ],
+      declarations: [
+      ],
+      providers: [
+        DomSanitizer,
+        SafeHtmlPipe
+       
+      ]
+    }).compileComponents();
+  }));
+
+  it('should be created', inject([SafeHtmlPipe], (pipe: SafeHtmlPipe) => {
     expect(pipe).toBeTruthy();
-  });
+  }));
+ 
 });
